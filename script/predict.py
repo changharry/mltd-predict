962e5c1c5ec647234160ab05ef536c23e848d03d
changharry <harrychangzh@gmail.com>
2022-07-05 13:50:38 +0800

    Initial commit

---
+++ script/predict.py
@@ -0,0 +1,51 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import requests
+from datetime import datetime
+import json
+import matplotlib.pyplot as plt
+
+# crontab task
+event_id = 241
+mr = requests.get(url="https://api.matsurihi.me/mltd/v1/events/{}".format(event_id)).json()
+start = mr["schedule"]["beginDate"]
+end = mr["schedule"]["endDate"]
+start_timestamp = datetime.timestamp(datetime.fromisoformat(start))
+end_timestamp = datetime.timestamp(datetime.fromisoformat(end))
+duration = (end_timestamp - start_timestamp) / 3600 / 24
+meta = {"name": mr["name"], "type": mr["type"]}
+regression_all = []
+for k in range(52):
+    endpoint = "https://api.matsurihi.me/mltd/v1/events/{}/rankings/logs/idolPoint/{}/10,100,1000".format(event_id,
+                                                                                                          k + 1)
+    r = requests.get(url=endpoint)
+    data = r.json()
+    regression = []
+    for j in range(len(data)):
+        x = []
+        y = []
+        for i in data[j]['data']:
+            y.append(i['score'])
+            x.append((datetime.timestamp(datetime.fromisoformat(i['summaryTime'])) - start_timestamp) / 3600 / 24)
+        x0 = np.array(x).reshape((-1, 1))
+        y0 = np.array(y)
+        model = LinearRegression().fit(x0, y0)
+        scatter = {"x": x, "y": y}
+        latestTime = datetime.fromtimestamp(x[-1] * 24 * 3600 + start_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+        prediction = round(model.intercept_ + model.coef_[0] * duration)
+        remain = round(duration - x[-1], 2)
+        last = {"idol_id": k + 1,
+                "latestTime": latestTime,
+                "remain": remain,
+                "latestScore": y[-1],
+                "prediction": prediction,
+                "scatter": scatter}
+        regression.append(last)
+    regression_all.append(regression)
+result = {"meta": meta, "regression_all": regression_all}
+with open('../data/data.json', 'w') as f:
+    json.dump(result, f)
+
+# plt.scatter(x, y, s=1)
+# plt.plot(x, model.predict(x0), c='r')
+# plt.show()
---
+++ view/views.py
@@ -0,0 +1,40 @@
+import json
+
+from flask import render_template
+
+from app import app
+
+
+@app.route('/')
+def index():
+    with open('data/data.json') as f:
+        data = json.load(f)
+        if data["meta"]["type"] == 5:
+            return render_template('anniversary.html')
+        else:
+            return "null"
+
+
+@app.route('/idol/<idol_id>')
+def idol(idol_id):
+    with open('data/data.json') as f:
+        data = json.load(f)
+        if data["meta"]["type"] == 5:
+            reg = data["regression_all"][int(idol_id) - 1]
+            meta = data["meta"]
+            return render_template('idol.html', para=[idol_dict[int(idol_id)-1], reg, len(reg), meta])
+        else:
+            return "null"
+
+
+idol_dict = ["天海 春香", "如月 千早", "星井 美希", "萩原 雪歩", "高槻 やよい",
+             "菊地 真", "水瀬 伊織", "四条 貴音", "秋月 律子", "三浦 あずさ",
+             "双海 亜美", "双海 真美", "我那覇 響", "春日 未来", "最上 静香",
+             "伊吹 翼", "田中 琴葉", "島原 エレナ", "佐竹 美奈子", "所 恵美",
+             "徳川 まつり", "箱崎 星梨花", "野々原 茜", "望月 杏奈", "ロコ",
+             "七尾 百合子", "高山 紗代子", "松田 亜利沙", "高坂 海美", "中谷 育",
+             "天空橋 朋花", "エミリー", "北沢 志保", "舞浜 歩", "木下 ひなた",
+             "矢吹 可奈", "横山 奈緒", "二階堂 千鶴", '馬場 このみ', "大神 環",
+             "豊川 風花", "宮尾 美也", "福田 のり子", "真壁 瑞希", "篠宮 可憐",
+             "百瀬 莉緒", "永吉 昴", "北上 麗花", "周防 桃子", "ジュリア",
+             "白石 紬", "桜守 歌織"]
