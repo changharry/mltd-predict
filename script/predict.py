c1c83613be230f8524c95201d8ac9cefa4e4b63f
changharry <harrychangzh@gmail.com>
2023-07-12 08:14:01 -0700

    update event id 292

--- script/predict.py
+++ script/predict.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import json
 
 # crontab task
-event_id = 288
+event_id = 292
 mr = requests.get(url="https://api.matsurihi.me/mltd/v1/events/{}".format(event_id)).json()
 start = mr["schedule"]["beginDate"]
 end = mr["schedule"]["endDate"]
