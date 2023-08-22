58dfb34c91b2e8872c4a7503ed742055c42c5ed6
changharry <harrychangzh@gmail.com>
2023-08-21 22:16:11 -0700

    crontab setting

--- script/predict.py
+++ script/predict.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import json
 
 # crontab task
+# 10,40 * * * * cd mltd-predict && venv/bin/python script/predict.py
 event_id = 296
 mr = requests.get(url="https://api.matsurihi.me/mltd/v1/events/{}".format(event_id)).json()
 start = mr["schedule"]["beginDate"]
