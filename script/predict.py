2bd7c97692cba72abde7c739e7febba20c81e686
changharry <harrychangzh@gmail.com>
2025-03-24 20:32:41 -0700

    UPDATE EVENT ID 373

--- script/predict.py
+++ script/predict.py
@@ -7,7 +7,7 @@ import json
 
 # crontab task
 # 10,40 * * * * cd mltd-predict && .venv/bin/python script/predict.py
-event_id = 371
+event_id = 373
 mr = requests.get(url="https://api.matsurihi.me/mltd/v1/events/{}".format(event_id)).json()
 start = mr["schedule"]["beginDate"]
 end = mr["schedule"]["endDate"]
